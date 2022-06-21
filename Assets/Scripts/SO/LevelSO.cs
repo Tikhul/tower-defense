5a5ac7727dd80f3fd5cc60388451e76d297d2189
Tikhova Uliana <tikhova.uliana@gmail.com>
2022-06-21 13:40:17 +0400

    Добавила название уровня

--- Assets/Scripts/SO/LevelSO.cs
+++ Assets/Scripts/SO/LevelSO.cs
@@ -5,8 +5,15 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "LevelScriptableObject", menuName = "ScriptableObjects/LevelSO", order = 6)]
 public class LevelSO : Config
 {
+    [SerializeField] private string _name;
     [SerializeField] private EnemyWaySO _enemyWay;
     [SerializeField] private List<WaveSO> _waves;
+
+    public string Name
+    {
+        get => _name;
+        set => _name = value;
+    }
 
     /// <summary>
     /// Конфиг маршрута, по которому будут двигаться враги в уровне
