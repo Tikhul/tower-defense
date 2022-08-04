844402090b20f0a2078abba3321137168cbe5d63
Tikhova Uliana <tikhova.uliana@gmail.com>
2022-08-04 12:17:25 +0400

    Начало создания нового инпута

---
+++ Assets/Scripts/Common/Input/InputView.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputView : MonoBehaviour
+{
+    [SerializeField] private Camera _camera;
+    [SerializeField] private KeyCode _shootKey;
+    [SerializeField] private KeyCode _menuKey;
+    void Update()
+    {
+        if (Input.GetKeyDown(_shootKey) || Input.GetKeyDown(_menuKey))
+        {
+            Ray ray = _camera.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray, out RaycastHit hit))
+            {
+                var interactable = hit.transform.GetComponentInParent<IInteractable>();
+                if(interactable != null && Input.GetKeyDown(_shootKey))
+                {
+                    interactable.TryShoot();
+                    Debug.Log("Shoot");
+                }
+                else if(interactable != null && Input.GetKeyDown(_menuKey))
+                {
+                    interactable.TryOpenMenu();
+                    Debug.Log("Menu");
+                }
+            }
+        }
+    }
+}
--- Assets/Scripts/Context/Level/LevelView/Cell/CellView.cs
+++ Assets/Scripts/Context/Level/LevelView/Cell/CellView.cs
@@ -1,8 +1,20 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CellView : BaseView
+public class CellView : BaseView, IInteractable
 {
-    
+    public event Action OnTryInteract;
+    public event Action OnInteract;
+
+    public void TryOpenMenu()
+    {
+        Debug.Log("TryOpenMenu");
+    }
+
+    public void TryShoot()
+    {
+        Debug.Log("TryShoot");
+    }
 }
