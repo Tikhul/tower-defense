32bf88a8325d4fdec6102b8e511163e5d2de0119
Tikhova Uliana <tikhova.uliana@gmail.com>
2022-08-08 13:49:45 +0400

    Правки TowerView

--- Assets/Scripts/Context/Level/LevelView/Towers/TowerView.cs
+++ Assets/Scripts/Context/Level/LevelView/Towers/TowerView.cs
@@ -1,4 +1,3 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using DG.Tweening;
@@ -33,15 +32,18 @@ public class TowerView : BaseView
     public string TowerRadiusText => _towerConfig.ShootRadius.ToString();
     public string TowerFrequencyText => _towerConfig.ShootDelay.ToString();
     public string TowerBulletsText => _towerConfig.BulletsNumber.ToString();
+
     private void OnEnable()
     {
         ShootsNumber = 0;
         SetRadiusCollider();
     }
+
     private void SetRadiusCollider()
     {
         _shootRadius.radius = _towerConfig.ShootRadius / 30;
     }
+
     /// <summary>
     /// Увеличение коллайдера, который показывает радиус стрельбы
     /// </summary>
@@ -49,12 +51,14 @@ public class TowerView : BaseView
     {
         _shootRadius.radius += upgrade / 30 - 30;
     }
+
     public void LaunchShooting(Dictionary<Vector3, EnemyView> receivedTransforms, TowerModel towerModel)
     {
         _cellView.Interactable = false;
         ShootsNumber += 1;
         TurnTower(receivedTransforms, towerModel);
     }
+
     public void TurnTower(Dictionary<Vector3, EnemyView> enemiesTransforms, TowerModel towerModel)
     {
         Vector3 nearestEnemy = enemiesTransforms.Keys.OrderBy(x => Vector3.Distance(transform.position, x)).First();
@@ -65,6 +69,7 @@ public class TowerView : BaseView
             .OnComplete(() => CreateBullet(towerModel, nearestEnemy));
         Debug.Log("Реальность " + enemiesTransforms[nearestEnemy].transform.position);
     }
+
     private void CreateBullet(TowerModel tower, Vector3 enemyTransform)
     {
   //      Debug.Log("CreateBullet");
@@ -73,37 +78,42 @@ public class TowerView : BaseView
         _newBullet.transform.localPosition = _bulletPrefab.transform.position;
         _newBullet.transform.localScale = _bulletPrefab.transform.localScale;
         _newBullet.GetComponent<BulletView>().BulletDamage = tower.Damage;
-        StartCoroutine(ShootBullet(_newBullet, tower, enemyTransform));
+        ShootBullet(_newBullet, tower, enemyTransform);
     }
-    private IEnumerator ShootBullet(GameObject newBullet, TowerModel tower, Vector3 enemyTransform)
+
+    private void ShootBullet(GameObject newBullet, TowerModel tower, Vector3 enemyTransform)
     {
  //       Debug.Log("ShootBullet");
         if(ShootsNumber == tower.BulletsNumber)
         {
-            newBullet.transform.DOLocalMoveY(
+            newBullet.transform.DOLocalMoveZ(
                 Vector3.Distance(transform.position, enemyTransform), _bulletTime)
                 .OnComplete(() => DestroyBullet(newBullet));
             RenewData();
         }
         else if(ShootsNumber < tower.BulletsNumber)
         {
-            yield return newBullet.transform.DOLocalMoveY(
+            newBullet.transform.DOLocalMoveZ(
                 Vector3.Distance(transform.position, enemyTransform), _bulletTime)
                 .OnComplete(() => AfterShoot(tower, newBullet));
         }
     }
+
     private void AfterShoot(TowerModel tower, GameObject bullet)
     {
         OnBulletShot?.Invoke(tower);
         DestroyBullet(bullet);
     }
+
     private void DestroyBullet(GameObject bullet)
     {
         if(bullet != null)
         {
+            bullet.transform.DOKill();
             Destroy(bullet);
         }
     }
+
     private void RenewData()
     {
         ShootsNumber = 0;
